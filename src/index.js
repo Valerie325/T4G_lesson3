c18f7469815e3e81a322361d66b027995af7a0b7
Valerie325 <morevimgh14@gmail.com>
2024-11-29 12:00:05 +0000

    added style

---
+++ src/index.js
@@ -0,0 +1,20 @@
+let x = prompt("give me a number");
+console.log(typeof{x});
+x = Number
+
+
+
+
+
+let del = prompt("Are you sure you want to delete");
+
+console.log(del);
+
+if (del === "yes") {
+    alert("deletion successful");
+} else {
+    alert("deletion unsuccessful");
+}
+
+
+ 
